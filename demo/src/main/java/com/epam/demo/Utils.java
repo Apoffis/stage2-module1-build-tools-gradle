5267575bdb21ec3a3079311ed8f770dfe801e889
Apoffis <abc22_08@mail.ru>
2022-11-08 23:39:16 +0400

    build-gradle commit 3

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,15 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.StringUtils;
+
+
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().noneMatch(a -> Integer.parseInt(a) < 0);
+        boolean res = args.stream().anyMatch(a -> !StringUtils.isNumeric(a) && Integer.parseInt(a) < 0);
+        return !res;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,10 @@
 package com.epam.utils;
 
+import java.util.List;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+
+        if (str == null || str.isEmpty()) return false;
 
         return Integer.parseInt(str) > 0;
     }
